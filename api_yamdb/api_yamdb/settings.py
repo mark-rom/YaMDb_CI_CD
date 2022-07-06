4620b15944d0870740e0325baccd31e78dcf6bdb
Павел Сергеев <pavel.al.sergeev@gmail.com>
2022-07-06 20:01:15 +0300

    fix static

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
