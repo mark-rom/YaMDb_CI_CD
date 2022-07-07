69c0a9cb6913b90eebe4112efa052cb5e2110aae
Павел Сергеев <pavel.al.sergeev@gmail.com>
2022-07-07 20:41:06 +0300

    fix base_import_command

--- api_yamdb/reviews/management/commands/base_import_command.py
+++ api_yamdb/reviews/management/commands/base_import_command.py
@@ -3,9 +3,9 @@ import os
 
 from django.core.management.base import BaseCommand, CommandError
 
-BASE_DIR = os.path.dirname(
+BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-)
+))
 
 
 class BaseImportCommand(BaseCommand):
